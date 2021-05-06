74aa6c3c31c9e2c2a7dc4a42ae95937210004415
ghunney <ghunney10@gmail.com>
2021-05-06 21:21:34 +0000

    Updates

--- src/data/footerData.js
+++
@@ -1,9 +0,0 @@
-import { FaFacebook, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
-
-
-export const socialsList = [
-    { name: 'facebook', path: "https://facebook.com/pghunney", icon: <FaFacebook /> },
-    { name: 'linkedin', path: "https://linkedin.com/in/papa-ghunney", icon: <FaLinkedin /> },
-    { name: 'instagram', path: "https://instagram.com/alphablaze_", icon: <FaInstagram /> },
-    { name: 'github', path: "https://github.com/papa-ghunney", icon: <FaGithub /> }
-]
--- src/data/menuData.js
+++
@@ -1,5 +0,0 @@
-export const menuData = [
-    { name: 'About', path: "/about" },
-    { name: 'Services', path: "/services" },
-    { name: 'Contact', path: "/contact" }
-]
